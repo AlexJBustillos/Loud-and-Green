fc78028826f834941cc5a4a9b73d6cb975a55057
alexjbustillos <alexjbustillos@gmail.com>
2020-11-30 11:25:09 -0500

    [config,migrations,models]updated to my project requirements

--- migrations/20201123164211-create-user.js
+++ migrations/20201123164211-create-user.js
@@ -17,6 +17,9 @@ module.exports = {
       password: {
         type: Sequelize.STRING
       },
+      age: {
+        type: Sequelize.INTEGER
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
--- models/user.js
+++ models/user.js
@@ -41,7 +41,16 @@ module.exports = (sequelize, DataTypes) => {
           msg: 'Password must be between 8 and 99 characters'
         }
       }
-    }
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      validate: {
+        min: {
+          args: 21,
+          msg: 'Must be 21yrs or older to Sign Up'
+        }
+      }
+    },
   }, {
     sequelize,
     modelName: 'user',
