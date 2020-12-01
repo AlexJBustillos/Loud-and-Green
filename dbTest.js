e9b25e99063dc23c039497e06fadd1b422e5a27f
alexjbustillos <alexjbustillos@gmail.com>
2020-12-01 10:20:55 -0500

    [dbTest.js]all models working with dbTest.js

--- dbTest.js
+++ dbTest.js
@@ -60,3 +60,76 @@ const API_KEY = process.env.API_KEY;
     //     console.log(err);
     // });
 
+// review model works
+// async function another() {
+//     const user = await db.user.findOne()
+//     const strain = await db.strain.findOne()
+//     const review = await db.review.create({
+//         content: 'This was really good',
+//         userId: user.id,
+//         name: 'Alex',
+//         strainId: strain.strainId
+//     })
+//     console.log(review);
+// }
+// another();
+
+// add strain to user
+// db.user.findOne({
+//     where: {
+//         name: 'Alex'
+//     }
+// }).then((alex) => {
+//     db.strain.findOne({
+//         where: {
+//             strainId: 20
+//         }
+//     }).then((foundStrain) => {
+//         alex.addStrain(foundStrain)
+//     })
+// })
+
+
+
+// add flavors to strain at flavors table
+// let strainId = 20;    
+// const url = `https://strainapi.evanbusse.com/${API_KEY}/strains/data/flavors/${strainId}`;
+//     axios.get(url)
+//     .then(response => {
+//         let flavors = response.data;
+//         console.log(flavors);
+//         db.strain.findOne({
+//             where: {
+//                 strainId: 20
+//             }
+//         }).then((foundStrain) => {
+//             db.flavor.create({
+//                 strainId: foundStrain.strainId,
+//                 flavors: flavors.toString()
+//             })
+//         })
+//     })
+
+// add effects to strain at strain table
+// let strainId = 20;
+// const url = `https://strainapi.evanbusse.com/${API_KEY}/strains/data/effects/${strainId}`;
+// axios.get(url)
+// .then(response => {
+//     const effects = response.data;
+//     // console.log(effects);
+//     for (const [key, value] of Object.entries(effects)) {
+//         let string = `${key}: ${value}`
+//         db.strain.findOne({
+//             where: {
+//                 strainId: 20
+//             }
+//         }).then((foundStrain) => {
+//             db.effect.create({
+//                 strainId: foundStrain.strainId,
+//                 effects: string
+//             })
+//         })
+//     }
+    
+// })
+
